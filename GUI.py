584d90ede32696e2b466c6b1237bf141b1105bb4
Rad-hi <radhisghaier33@gmail.com>
2021-05-02 22:00:38 +0100

    Added color picking functionality so now the user could choose what color each plotted value is assigned

--- GUI.py
+++ GUI.py
@@ -3,7 +3,7 @@
 from parser import parser
 
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, colorchooser
 from tkcalendar import DateEntry
 
 import numpy as np
@@ -16,6 +16,7 @@ style.use('ggplot')
 
 class GUI(tk.Tk):
     ''' This is the showroom, where all graphic elements are created '''
+    
     BACKGROUND_COLOR    = '#131313'
     COMMON_Y_POS        = 0.925
     COMMON_X_POS        = 0.865
@@ -25,6 +26,9 @@ class GUI(tk.Tk):
     MAX_MIN_MEAN        = (0)*3 
     # Holds the latest picked date
     DATE                = ('')*3
+    # Holds the latest state of the max_col, min_col, mean_col
+    COLORS              = ('')*3
+
     def __init__(self, title='Temp_log', min_size=(1120,630),
                  fg='white', canvas_colour='white'):
         super().__init__()
@@ -55,6 +59,7 @@ class GUI(tk.Tk):
         self._create_load_csv_button()
         self._create_fetching_controls()
         self._create_mx_mi_mn_checkbuttons()
+        self._create_color_pickers()
 
     def _create_day_button(self):
         ttk.Button(self, text="Day", command=self._draw_day)\
@@ -66,7 +71,7 @@ class GUI(tk.Tk):
 
     def _create_month_button(self):
         ttk.Button(self, text="Month", command=self._draw_month)\
-           .place(relx=0.24, rely=self.COMMON_Y_POS, relwidth=0.1)
+           .place(relx=0.239, rely=self.COMMON_Y_POS, relwidth=0.1)
 
     def _create_mx_mi_mn_checkbuttons(self): # Add commands to update the graph automatically ?
         tk.Label(self, text="Choose value(s) to graph:", 
@@ -87,6 +92,58 @@ class GUI(tk.Tk):
         ttk.Checkbutton(self, text="Mean", variable=self._check_Mean, 
                         onvalue=True, offvalue=False)\
            .place(relx=0.624, rely=self.COMMON_Y_POS, relheight=0.042, relwidth=0.05)
+
+    def _create_color_pickers(self):
+        self.max_color  = tk.StringVar()
+        self._max_btn   = tk.Button(self, text="", command=self._pick_color_max, relief='flat')
+        self._max_btn.place(relx=0.515, rely=self.COMMON_Y_POS+0.05, relheight=0.015, relwidth=0.05)
+
+        self.min_color  = tk.StringVar()
+        self._min_btn   = tk.Button(self, text="", command=self._pick_color_min, relief='flat')
+        self._min_btn.place(relx=0.57, rely=self.COMMON_Y_POS+0.05, relheight=0.015, relwidth=0.05)
+        
+        self.mean_color = tk.StringVar()
+        self._mean_btn  = tk.Button(self, text="", command=self._pick_color_mean, relief='flat')
+        self._mean_btn.place(relx=0.624, rely=self.COMMON_Y_POS+0.05, relheight=0.015, relwidth=0.05)
+
+        self._reset_color_pickers()
+
+    def _pick_color_max(self):
+        self._pick_color("max")
+    
+    def _pick_color_min(self):
+        self._pick_color("min")
+
+    def _pick_color_mean(self):
+        self._pick_color("mean")    
+
+    def _pick_color(self, picker):
+        col = colorchooser.askcolor(initialcolor='#000000') # Change color here
+        # print(col[1]) # Returns a tuple --> ((255, 255, 255),'#ffffff')
+        
+        # If a user cancels in the middle of choosing a color, an empty string is returned
+        if(col[1]):  
+            if(picker == "max"):        
+                self.max_color.set(col[1])
+                self._max_btn['bg']  = col[1]
+            elif(picker == "min"):
+                self.min_color.set(col[1])
+                self._min_btn['bg']  = col[1]
+            else:
+                self.mean_color.set(col[1])
+                self._mean_btn['bg'] = col[1]
+   
+    def _get_colors(self):
+        return self.max_color.get(), self.min_color.get(), self.mean_color.get()
+
+    def _reset_color_pickers(self, mx="#50514f", mi="#cb904d", mn="#71a2b6"):
+        # Default plotting colors
+        self.max_color.set(mx)
+        self.min_color.set(mi)
+        self.mean_color.set(mn)
+        self._max_btn['bg']  = mx
+        self._min_btn['bg']  = mi
+        self._mean_btn['bg'] = mn
 
     def _create_date_picker(self):
         tk.Label(self, text="Choose date:", 
@@ -148,50 +205,26 @@ class GUI(tk.Tk):
         self._assign_drawing("M")
 
     def _assign_drawing(self, picker):
-        mx, mi, mn = self._get_checks()
-        d, m, y    = self._get_date()
+        mx, mi, mn    = self._get_checks()
+        d, m, y       = self._get_date()
+        cmx, cmi, cmn = self._get_colors()
         if(self._parser.FETCHED and\
           (self.CURRENT_GRAPH != picker or\
           (mx, mi, mn) != self.MAX_MIN_MEAN or\
-          (d, m, y) != self.DATE)):
+          (d, m, y) != self.DATE or\
+          (cmx, cmi, cmn) != self.COLORS)):
             
             self._draw(self._parser._df, (mx, mi, mn), kind=picker, date=(d, m, y))
             self.CURRENT_GRAPH = picker
             self.MAX_MIN_MEAN = (mx, mi, mn)
             self.DATE = (d, m, y)
-
-    def _authenticate(self, *args):
-        user, feed, key = self._get_inputs()   
-        if(len(user) and len(feed) and len(key)):
-            self._parser.fetch_data(user, feed, key)
-            self._reset_inputs()
-        else:
-            print("Type in Something") # Make it graphic!
-
-    def _reset_inputs(self):
-        self.USER.set("")
-        self.FEED.set("")
-        self.KEY.set("") 
-
-    def _get_inputs(self):
-        return self.USER.get(), self.FEED.get(), self.KEY.get()
-
-    def _get_checks(self):
-        return self._check_Max.get(), self._check_Min.get(), self._check_Mean.get()
-
-    def _get_date(self):
-        m, d, y = self._calendar.get().split('/')
-        # Don't think we'll read anything after 2050, or before 1950 \__(°_°)__/
-        if(int(y) < 50): 
-            y = "20"+y
-        else:
-            y = "19"+y
-        return int(d), int(m), int(y)
+            self.COLORS = (cmx, cmi, cmn)
 
     # Data ; values(max, min, mean) ; kind=day/week/month ; date(day, month, year)
     def _draw(self, data, values, kind="D", date=(0, 0, 0)):
         mx_mi_mn = ["mx", "mi", "mn"]
-        colours_ = ['#131313', (0,1,0), (0,0,1)] #customize these!?
+        colours_ = [0, 0, 0] #gotta find a better way to do this!
+        colours_[0], colours_[1], colours_[2] = self._get_colors() 
         self._fig.clear()
 
         if(kind == "D"): # Plotting a day
@@ -203,6 +236,9 @@ class GUI(tk.Tk):
                     self._fig.plot(printable[mx_mi_mn[i]], color=colours_[i])
         
         elif(kind == "W"): # Plotting a week 
+            
+            ''' Plotting a week is complicated since we need to check 
+            if we're in the end of a month and wrap the "day" value '''
             self._fig.plot([1,2,3,4,5,6,7,8],[20,-12,9,8,1,9,3,-6])
         
         else: # Plotting a month
@@ -224,6 +260,36 @@ class GUI(tk.Tk):
         _canvas = FigureCanvasTkAgg(self._graph, self)
         _canvas.get_tk_widget()\
                .place(relx=0.03, rely=0.052, relheight=0.85, relwidth=0.8)
+
+    def _authenticate(self, *args):
+        user, feed, key = self._get_inputs()   
+        if(len(user) and len(feed) and len(key)):
+            self._parser.fetch_data(user, feed, key)
+            self._reset_inputs()
+        else:
+            print("Type in Something") # Make it graphic!
+
+    def _reset_inputs(self):
+        self.USER.set("")
+        self.FEED.set("")
+        self.KEY.set("") 
+
+
+    def _get_inputs(self):
+        return self.USER.get(), self.FEED.get(), self.KEY.get()
+
+    def _get_checks(self):
+        return self._check_Max.get(), self._check_Min.get(), self._check_Mean.get()
+
+    def _get_date(self):
+        m, d, y = self._calendar.get().split('/')
+        # Don't think we'll read anything after 2050, or before 1950 \__(°_°)__/
+        if(int(y) < 50): 
+            y = "20"+y
+        else:
+            y = "19"+y
+        return int(d), int(m), int(y)
+
 
     def _save_csv(self):
         self._parser.output_csv()
