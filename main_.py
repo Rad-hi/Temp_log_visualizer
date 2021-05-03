24bb14af9959ad6ed2582c974dd7d9c7f60b680a
Rad-hi <radhisghaier33@gmail.com>
2021-05-03 01:02:34 +0100

    cleanups

--- main_.py
+++ main_.py
@@ -3,12 +3,14 @@
 from GUI import GUI
 
 class main_:
+    ''' Class that launches everything '''
+
     def __init__(self, *args, **kwargs):
-        self._initialise_display(*args, **kwargs)
+        self._initialise_gui(*args, **kwargs)
         self._update_display()
         self._gui.mainloop()
 
-    def _initialise_display(self, *args, **kwargs):
+    def _initialise_gui(self, *args, **kwargs):
         self._gui = GUI(*args, **kwargs)
 
     def _update_display(self):
